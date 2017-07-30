23213e541d631a9a68c950da995e5d84e0e30fdf
PRDBX1 <motya770@gmail.com>
2017-07-30 22:59:31 +0300

    1) fixed left outer join in stockrepository

--- src/main/java/com/dtrade/repository/stock/StockRepository.java
+++ src/main/java/com/dtrade/repository/stock/StockRepository.java
@@ -4,6 +4,8 @@ import com.dtrade.model.account.Account;
 import com.dtrade.model.diamond.Diamond;
 import com.dtrade.model.stock.Stock;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -16,5 +18,6 @@ public interface StockRepository  extends JpaRepository<Stock, Long>{
 
     Stock findByAccountAndDiamond(Account account, Diamond diamond);
 
-    List<Stock> findByAccount(Account account);
+    @Query("select s from Stock s where s.account.id =  :#{#account.id}")
+    List<Stock> findByAccount(@Param("account") Account account);
 }
