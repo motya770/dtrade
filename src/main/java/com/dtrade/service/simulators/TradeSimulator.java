aadd8424b7fb120f5209203cea63c96681cdd073
PRDBX1 <motya770@gmail.com>
2018-04-21 22:41:12 +0300

    1) removed unused properties 2) added basic simulation 1.0

--- src/main/java/com/dtrade/service/simulators/TradeSimulator.java
+++ src/main/java/com/dtrade/service/simulators/TradeSimulator.java
@@ -1,19 +1,36 @@
 package com.dtrade.service.simulators;
 
 
+import com.dtrade.model.account.Account;
+import com.dtrade.model.diamond.Diamond;
+import com.dtrade.model.quote.Quote;
+import com.dtrade.model.tradeorder.TradeOrder;
+import com.dtrade.model.tradeorder.TradeOrderType;
 import com.dtrade.service.IAccountService;
+import com.dtrade.service.IDiamondService;
+import com.dtrade.service.IQuotesService;
 import com.dtrade.service.ITradeOrderService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Profile;
 import org.springframework.core.env.Environment;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 
 import javax.annotation.PostConstruct;
+import java.math.BigDecimal;
+import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
-//@Component
-@Profile("local")//maybe too much?)
+@Component
 public class TradeSimulator {
 
     private static final Logger logger = LoggerFactory.getLogger(TradeSimulator.class);
@@ -27,24 +44,107 @@ public class TradeSimulator {
     @Autowired
     private Environment environment;
 
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    private IDiamondService diamondService;
+
+    @Autowired
+    private IQuotesService quotesService;
+
+    //two different logins f
     @PostConstruct
     private void init(){
 
-         String simulateTrade = environment.getProperty("simulateTrade");
+         String simulateTrade = environment.getProperty("trade.simulateTrade");
          if(StringUtils.isEmpty(simulateTrade)){
              logger.info("Simulation is disabled");
              return;
          }
 
          if(Boolean.valueOf(simulateTrade).equals(Boolean.TRUE)){
+            logger.info("Starting simulation....");
 
-             System.out.println("starting simulation....");
+            Runnable r1 = getRunnable("motya770@gmail.com");
+            Runnable r2 = getRunnable("test@test.com");
 
-             //IAccountService accountService = accountService.getCurrentAccount();
+            ScheduledExecutorService executorService = Executors.newScheduledThreadPool(6);
+            executorService.scheduleAtFixedRate(r1, 100, 3_000, TimeUnit.MILLISECONDS);
+            executorService.scheduleAtFixedRate(r2, 100, 3_000, TimeUnit.MILLISECONDS);
 
          }else{
              logger.info("Simulation is disabled");
          }
+    }
+
+    private Runnable getRunnable(String userName){
+        Runnable r1 = ()->{
+            logger.info("First account simulation");
+            try{
+                startTrade(userName);
+                Thread.currentThread().sleep(1_000);}
+            catch (Exception e){
+                logger.error("{}", e);
+            }
+        };
+        return r1;
+    }
+
+    private void startTrade(String accountName){
+        login(accountName);
+        Account account = accountService.getStrictlyLoggedAccount();
+        if(account.getBalance().compareTo(new BigDecimal("0.0"))==-1) {
+            accountService.updateBalance(account, new BigDecimal("10000"));
+        }
+        createTradeOrderSimulated();
+    }
+
+    private TradeOrder createTradeOrderSimulated() {
+
+        Diamond diamond = diamondService.getAllAvailable("").stream().findFirst().get();
+        Quote quote = quotesService.getLastQuote(diamond);
+
+        Random rand = new Random();
+        int random = rand.nextInt(2);
+
+        logger.info("rand value " + random);
+        //random buy and random sell (simulation!! :-))
+        TradeOrderType tradeOrderType = (random == 0) ? TradeOrderType.BUY : TradeOrderType.SELL;
+
+        BigDecimal price = null;
+
+        int tradeOrderTypeRandom = rand.nextInt(2);
+        BigDecimal multiplicator = tradeOrderTypeRandom == 0 ? new BigDecimal("1.05") : new BigDecimal("0.95") ;
+
+        if(tradeOrderType.equals(TradeOrderType.BUY)){
+            price = quote.getAsk();
+        }else{
+            price = quote.getBid();
+        }
+
+        price = price.multiply(multiplicator);
+
+        if(price==null){
+            price = new BigDecimal("100.00");
+        }
+
+        TradeOrder tradeOrder = new TradeOrder();
+        tradeOrder.setAmount(new BigDecimal("10.0"));
+        tradeOrder.setDiamond(diamond);
+        tradeOrder.setAccount(accountService.getCurrentAccount());
+        tradeOrder.setPrice(price);
+
+        tradeOrder.setTradeOrderType(tradeOrderType);
+        tradeOrder = tradeOrderService.createTradeOrder(tradeOrder);
+        return tradeOrder;
+    }
+
+    private void login(String userName){
+
+        UserDetails userDetails = userDetailsService.loadUserByUsername (userName);
+        Authentication auth = new UsernamePasswordAuthenticationToken(userDetails, userDetails.getPassword (),userDetails.getAuthorities ());
+        SecurityContextHolder.getContext().setAuthentication(auth);
 
     }
 }
