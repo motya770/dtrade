a07cdc06b64624b42d3e048329f045af21cade56
PRDBX1 <motya770@gmail.com>
2018-04-25 10:31:27 +0300

    1) fixed bug in pom to docker 2) fixing class not found bug

--- src/main/java/com/dtrade/model/bookorder/BookOrderView.java
+++ src/main/java/com/dtrade/model/bookorder/BookOrderView.java
@@ -2,10 +2,7 @@ package com.dtrade.model.bookorder;
 
 import com.dtrade.model.tradeorder.TradeOrder;
 import com.fasterxml.jackson.annotation.JsonRootName;
-import javafx.util.Pair;
 import lombok.Data;
-
-import java.math.BigDecimal;
 import java.util.List;
 
 @Data
@@ -14,8 +11,6 @@ public class BookOrderView {
 
     private List<TradeOrder> buyOrders;
     private List<TradeOrder> sellOrders;
-
-    private Pair<BigDecimal, BigDecimal> spread;
 
     public BookOrderView(List<TradeOrder> buyOrders, List<TradeOrder> sellOrders){
         this.buyOrders = buyOrders;
