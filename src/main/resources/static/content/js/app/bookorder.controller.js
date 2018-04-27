5eccd89fdda4c6d1b72daa4e90987d41738d185c
PRDBX1 <motya770@gmail.com>
2018-04-27 15:37:52 +0300

    1) fixed bookorder

--- src/main/resources/static/content/js/app/bookorder.controller.js
+++ src/main/resources/static/content/js/app/bookorder.controller.js
@@ -15,7 +15,7 @@ diamondApp.controller("BookOrderController", function BookOrderController($scope
             self.bookOrder = data;
 
             if(self.bookOrder.buyOrders.length > 0 && self.bookOrder.sellOrders.length > 0) {
-                self.spread = self.bookOrder.buyOrders[self.bookOrder.buyOrders.length - 1].price - self.bookOrder.sellOrders[0].price;
+                self.spread = self.bookOrder.sellOrders[0].price - self.bookOrder.buyOrders[self.bookOrder.buyOrders.length - 1].price;
                 console.log("spread: " + self.spread  + " " + self.bookOrder.buyOrders[self.bookOrder.buyOrders.length - 1].price  + " " + self.bookOrder.sellOrders[0].price);
             }
             if(self.firstTimeOut) {
