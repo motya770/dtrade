7545ae8b7f70e2d9103aa66981dbf829eb5330e7
PRDBX1 <motya770@gmail.com>
2018-07-07 23:00:35 +0300

    1) removed stackyfy

--- wrapper.sh
+++ wrapper.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-wget https://s1.stackify.com/Account/AgentDownload/Linux --output-document=stackify.tar.gz \
-     && tar -zxvf stackify.tar.gz stackify-agent-install-32bit && cd stackify-agent-install-32bit \
-     && sudo ./agent-install.sh --key 3Yr0Mq3Vt4As8Eq5Jw5Al0Rz1Ka0Tr7Fo8Mu3Rz --environment Production
-
-
 while ! exec 6<>/dev/tcp/${DATABASE_HOST}/${DATABASE_PORT}; do
     echo "Trying to connect to MySQL at ${DATABASE_PORT}..."
     sleep 10
