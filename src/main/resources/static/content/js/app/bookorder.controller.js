8f2e461e60253e10839d439da389964c120c661a
PRDBX1 <motya770@gmail.com>
2018-07-18 12:38:03 +0300

    1) fixed spread

--- src/main/resources/static/content/js/app/bookorder.controller.js
+++ src/main/resources/static/content/js/app/bookorder.controller.js
@@ -2,7 +2,7 @@ diamondApp.controller("BookOrderController", function BookOrderController($scope
 
     var self = this;
     self.firstTimeOut = true;
-    self.spread = 0;
+    self.spread = 0.00;
 
     var callBookOrderService = function() {
         if(DiamondService.getCurrentDiamond()==null){
@@ -19,17 +19,20 @@ diamondApp.controller("BookOrderController", function BookOrderController($scope
                 return;
             }
 
-            if(self.bookOrder.buyOrder == null || self.bookOrder.sellOrders==null){
+            if(self.bookOrder.buyOrders == null || self.bookOrder.sellOrders==null){
 
-                self.spread = 0;
+                self.spread = 0.00;
 
             }else if (self.bookOrder.buyOrders.length > 0 && self.bookOrder.sellOrders.length > 0) {
                 self.spread = self.bookOrder.sellOrders[self.bookOrder.sellOrders.length - 1].price -
                     self.bookOrder.buyOrders[0].price;
+               // console.log("calc")
                // console.log("spread: " + self.spread  + " " + self.bookOrder.buyOrders[self.bookOrder.buyOrders.length - 1].price  + " " + self.bookOrder.sellOrders[0].price);
             } else {
-                self.spread = 0;
+                self.spread = 0.00;
             }
+
+           // console.log("spd: " + (self.bookOrder.sellOrders[self.bookOrder.sellOrders.length - 1].price - self.bookOrder.buyOrders[0].price) + " " + self.spread);
 
             if(self.firstTimeOut) {
                 self.firstTimeOut = false;
