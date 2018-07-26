5b9011d81f67463d76e5517bee52684022f3aaf4
PRDBX1 <motya770@gmail.com>
2018-07-26 13:35:45 +0300

    1) allowed period

--- src/main/java/com/dtrade/WebSecurityConfig.java
+++ src/main/java/com/dtrade/WebSecurityConfig.java
@@ -6,12 +6,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
 import org.springframework.security.crypto.password.MessageDigestPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.firewall.HttpFirewall;
+import org.springframework.security.web.firewall.StrictHttpFirewall;
 
 /**
  * Created by kudelin on 8/24/16.
@@ -20,6 +23,22 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+    @Bean
+    public HttpFirewall allowUrlEncodedSlashHttpFirewall() {
+        StrictHttpFirewall firewall = new StrictHttpFirewall();
+        firewall.setAllowUrlEncodedPeriod(true);
+
+        return firewall;
+    }
+
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        super.configure(web);
+        web.httpFirewall(allowUrlEncodedSlashHttpFirewall());
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -62,7 +81,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
         http.headers()
                 .frameOptions().disable();
+
+
     }
+
+
+
 
     //TODO migrate
     @Bean
