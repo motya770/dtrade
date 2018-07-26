89ea7c934d9a3e46cc702e7cd2e502bba3493a7f
PRDBX1 <motya770@gmail.com>
2018-07-26 14:18:50 +0300

    1) allowed cors

--- src/main/java/com/dtrade/WebConfig.java
+++ src/main/java/com/dtrade/WebConfig.java
@@ -3,7 +3,9 @@ package com.dtrade;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
@@ -12,14 +14,18 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @Configuration
 @ComponentScan
 @EnableCaching
-public class WebConfig extends WebMvcConfigurerAdapter {
+public class WebConfig implements WebMvcConfigurer {
 
-    @Override
    public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/diamonds").setViewName("diamonds");
         registry.addViewController("/crypto").setViewName("diamonds");
         registry.addViewController("/account").setViewName("account");
         registry.addViewController("/trade").setViewName("index2");
         //registry.addViewController("/customer").setViewName("customer");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
     }
 }
