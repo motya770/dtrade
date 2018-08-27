339b40928d683c651ca22cdc0949cf2c1ac7bc28
Arctic Fox <fox@x1team.com>
2018-08-27 23:18:16 +0300

    1) changes restart

--- restart.sh
+++ restart.sh
@@ -1,5 +1,5 @@
 cd /code/dtrade_repo/
-git pull https://motya770:pragma1pragma1@bitbucket.org/crazy_team_007/dtrade_repo.git
+git pull https://jenkins:hjjxi~S[(X[z/2]X`;7n{@gitlab.x1team.com/fox/dtrade.git
 mvn clean install -DskipTests dockerfile:build dockerfile:push
 cd /work
 docker-compose down
