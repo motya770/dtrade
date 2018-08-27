9fa602be6bce9202b3e28f046132ad61f4bac39c
Arctic Fox <fox@x1team.com>
2018-08-27 23:32:22 +0300

    1) fix restart 2.0

--- restart.sh
+++ restart.sh
@@ -1,7 +1,6 @@
 cd /code/dtrade_repo/dtrade/
 git pull https://jenkins@gitlab.x1team.com/fox/dtrade.git
 mvn clean install -DskipTests dockerfile:build dockerfile:push
-cd /work
 docker-compose down
 docker-compose up
 
