531bea87b86c3bcd09af792d30a09106a9506faa
Arctic Fox <fox@x1team.com>
2018-12-10 20:05:39 +0200

    1) fixed cross origin for prices

--- platform/src/main/java/com/dtrade/controller/QuoteController.java
+++ platform/src/main/java/com/dtrade/controller/QuoteController.java
@@ -10,10 +10,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.util.Pair;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
@@ -33,6 +30,7 @@ public class QuoteController {
     @Autowired
     private IQuotesServiceProxy quotesServiceProxy;
 
+    @CrossOrigin
     @RequestMapping(value = "/get-landing-quotes", method = RequestMethod.POST)
     public Map<String, String> getLandingQuotes() throws TradeException {
         return quotesService.getLandingQuotes();
