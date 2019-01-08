71f1a94c33bb24eb906354fc5dc5a6e4e4a34448
Arctic Fox <fox@x1team.com>
2019-01-08 22:10:10 +0200

    1) fixed caption MY COINs and STOCks

--- common/src/main/java/com.dtrade/model/currency/Currency.java
+++ common/src/main/java/com.dtrade/model/currency/Currency.java
@@ -1,16 +1,51 @@
 package com.dtrade.model.currency;
 
+import com.fasterxml.jackson.annotation.JsonValue;
+
 //do not edit names !!!
 public enum  Currency {
-    USD(true), BTC(true), ETH(true),
-    LTC, BHC, ETC,
-    APPLE, MICROSOFT, FACEBOOK, GOOGLE, TESLA,
-    AMAZON, INVESCO, NETFLIX, AMD, NVIDIA, INTEL, BROADCOM, CISCO,
-    MICRON, ALIBABA, BRISTOL, BANK_OF_AMERICA, JP_MORGAN,
-    VISA, BOENG, CITY_GROUP, UNITED_HELTH, BERKSHIRE, AT_T,
-    EXXON_MOBIL, JOHNSON_JOHNSON,
-    WELLS_FARGO, DISNEY, PFIZER, MERCK,
-    GENERAL_ELECTRIC, VERIZON, SALESFORCE, BARCLAYS, COCA_COLA, EASY_JET;
+    USD(true, "US Dollar"),
+    BTC( "Bitcoin"),
+    ETH( "Ethereum"),
+    LTC("Litecoin"),
+    BHC("Bitcoin Cash"),
+    ETC("Ethereum Classic"),
+    APPLE("Apple"),
+    MICROSOFT("Microsoft"),
+    FACEBOOK("Facebook"),
+    GOOGLE("Google"),
+    TESLA("Tesla"),
+    AMAZON("Amazon"),
+    INVESCO("Invesco"),
+    NETFLIX("Netflix"),
+    AMD("AMD"),
+    NVIDIA("Nvidia"),
+    INTEL("Intel"),
+    BROADCOM("Broadcom"),
+    CISCO("Cisco"),
+    MICRON("Micron"),
+    ALIBABA("Alibaba"),
+    BRISTOL("Bristol"),
+    BANK_OF_AMERICA("Bank of America"),
+    JP_MORGAN("JPMorgan Chase"),
+    VISA("Visa"),
+    BOENG("Boeing"),
+    CITY_GROUP("City Group"),
+    UNITED_HELTH("UnitedHealth Group"),
+    BERKSHIRE("Berkshire Hathaway"),
+    AT_T("AT&T"),
+    EXXON_MOBIL("Exxon Mobil"),
+    JOHNSON_JOHNSON("Johnson Johnson"),
+    WELLS_FARGO("Wells Fargo"),
+    DISNEY("Disney"),
+    PFIZER("Pfizer"),
+    MERCK("Merck & Co"),
+    GENERAL_ELECTRIC("General Electric"),
+    VERIZON("Verizon"),
+    SALESFORCE("Salesforce"),
+    BARCLAYS("Barclays"),
+    COCA_COLA("Coca Cola"),
+    EASY_JET("Easy Jet");
 
     Currency(){
         this.baseCurrency = false;
@@ -20,9 +55,31 @@ public enum  Currency {
         this.baseCurrency = baseCurrency;
     }
 
-    private final boolean baseCurrency;
+    Currency(boolean baseCurrency, String name) {
+        this.baseCurrency = baseCurrency;
+        this.name  = name;
+    }
+
+    Currency(String name) {
+        this.name = name;
+    }
+
+
+    private String name;
+
+    private boolean baseCurrency;
+
+    @JsonValue
+    public String getName(){
+        return name;
+    }
 
     public boolean isBaseCurrency() {
         return baseCurrency;
+    }
+
+    @Override
+    public String toString() {
+        return name;
     }
 }
