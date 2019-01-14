a13a48977921ba3e5c8d07fea5d4670ccc8a9636
Arctic Fox <fox@x1team.com>
2019-01-14 21:19:13 +0200

    1) reactive bookorder

--- platform/src/main/java/com/dtrade/controller/BookOrderProxyController.java
+++ platform/src/main/java/com/dtrade/controller/BookOrderProxyController.java
@@ -1,19 +1,15 @@
 package com.dtrade.controller;
 
 import com.dtrade.model.bookorder.BookOrderView;
-import com.dtrade.model.diamond.Diamond;
 import com.dtrade.service.IBookOrderServiceProxy;
-import com.dtrade.utils.MyPair;
-import org.apache.commons.lang3.tuple.Pair;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.CompletableFuture;
 
 @RestController
 @RequestMapping(value = "/book-order", method = RequestMethod.POST)
@@ -23,20 +19,12 @@ public class BookOrderProxyController {
     private IBookOrderServiceProxy bookOrderServiceProxy;
 
     @RequestMapping(value = "/get-diamonds-spread", method = RequestMethod.POST)
-    public CompletableFuture<String> getLastQuotesForDiamonds(@RequestBody(required = true) ArrayList<Long> diamonds){
-
-        return CompletableFuture.supplyAsync(() -> {
-                return bookOrderServiceProxy.getSpreadForDiamonds(diamonds);
-            }
-        );
+    public Mono<String> getLastQuotesForDiamonds(@RequestBody(required = true) ArrayList<Long> diamonds){
+        return bookOrderServiceProxy.getSpreadForDiamonds(diamonds);
     }
 
     @RequestMapping(value = "/")
-    public CompletableFuture<BookOrderView> getBookOrder(@RequestBody Long diamondId){
-
-        return CompletableFuture.supplyAsync(() -> {
-                return bookOrderServiceProxy.getBookOrderView(diamondId);
-            }
-        );
+    public Mono<BookOrderView> getBookOrder(@RequestBody Long diamondId){
+        return bookOrderServiceProxy.getBookOrderView(diamondId);
     }
 }
