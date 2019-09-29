d13950106420b733a0ced431b954deea9f35bffe
Arctic Fox <fox@x1team.com>
2019-09-29 10:50:00 +0300

    1) added cache for consul

--- common/src/main/java/com.dtrade/utils/ConsulUtils.java
+++ common/src/main/java/com.dtrade/utils/ConsulUtils.java
@@ -2,6 +2,7 @@ package com.dtrade.utils;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.stereotype.Component;
 
@@ -11,6 +12,7 @@ public class ConsulUtils {
     @Autowired
     private DiscoveryClient discoveryClient;
 
+    @Cacheable(value = "engineUrl")
     public String engineUrl() {
         return discoveryClient.getInstances("engine")
                 .stream()
